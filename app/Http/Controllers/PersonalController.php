edddcd8a19eaa8f489de3abdf0c8d49d2eda0d04
Reynaldo <reynaldex100@gmail.com>
2020-07-25 14:43:18 -0400

    Backend de Personal

---
+++ app/Http/Controllers/PersonalController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Personal;
+use Illuminate\Http\Request;
+
+class PersonalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $personal = Personal::get();
+        echo json_encode($personal);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $personal = new Personal();
+        $personal->carnet = $request->input('carnet');
+        $personal->nombres = $request->input('nombres');
+        $personal->apellidos = $request->input('apellidos');
+        $personal->item = $request->input('item');
+        $personal->cargo = $request->input('cargo');
+        $personal->save();
+        echo json_encode($personal);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Personal  $personal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Personal $personal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Personal  $personal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Personal $personal)
+    {
+        //
+    }
+}
